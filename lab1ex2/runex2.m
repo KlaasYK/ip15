ac205b6cf0a035fe1446982425c312f5cb4fd379
KlaasYK <klaasyk@users.noreply.github.com>
2015-11-25 20:34:31 +0100

    Lab 1 ex 2 done

--- lab1ex2/runex2.m
+++ lab1ex2/runex2.m
@@ -3,23 +3,12 @@ x = imread('../images/fracturedspine.tiff');
 
 hist = IPhistogram(x);
 bar(hist);
+axis("tight");
+axis("tic", "labely");
 eqimg = IPhisteq(x,hist);
 imwrite(eqimg, 'test.tiff');
 figure;
 hist2 = IPhistogram(eqimg);
 bar(hist2);
-
-%figure;
-%eqimg2 = IPhisteq(eqimg, hist2);
-%hist3 = IPhistogram(eqimg2);
-%bar(hist3);
-
-
-
-
-%hist2 = IPhistogram(eqimg);
-%figure;
-%bar(hist);
-%figure;
-%bar(hist2);
-%
+axis("tight");
+axis("tic", "labely");
