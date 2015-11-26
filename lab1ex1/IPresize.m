b68337c1715e6fd038ea04ffe65130d7d7f9bac3
KlaasYK <klaasyk@users.noreply.github.com>
2015-11-26 15:24:50 +0100

    Report 1 sort of finished

--- lab1ex1/IPresize.m
+++ lab1ex1/IPresize.m
@@ -3,17 +3,17 @@ function out = IPresize(img,factor)
 if (factor < 0)
   factor *= -1;
   new = uint8(zeros(size(img)/factor));
-  
+
   for i = 1 : columns(new)
     for j = 1 : rows(new)
        new(j,i) = img(j*factor,i*factor);
     endfor
   endfor
-else 
+else
   new = uint8(zeros(size(img)*factor));
   % because matlabs starts at 1....
   climit = columns(new)-factor;
-  rlimit = rows(new)-factor;  
+  rlimit = rows(new)-factor;
   for i = 1 : climit
     for j = 1 : rlimit
       new(j,i) = img(floor(j/factor)+1,floor(i/factor)+1);
