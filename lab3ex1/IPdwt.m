08c2c25bde374dcb349b2ca3471878282c3ca73f
KlaasYK <klaasyk@users.noreply.github.com>
2015-12-04 12:43:06 +0100

    Start lab3

---
+++ lab3ex1/IPdwt.m
@@ -0,0 +1,20 @@
+% function to perform 1D Haar wavelet transform
+function retval = IPdwt(x,s) 
+sqrt2 = sqrt(2);
+out = x;
+
+initl = length(out);
+
+for i = 1 : s
+  % Calculate the means and details
+  odds = out(1:2:initl);
+  evens = out(2:2:initl);
+  sums = (odds + evens);
+  diffs = (odds - evens);
+  % Put the new values
+  out(1:initl) = [sums, diffs] / sqrt2;
+  initl = initl / 2
+end
+
+retval = out;
+end
