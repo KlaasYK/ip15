1f70baa2d1be50db26772b6d27c8e9988ccbb6d2
Jan Kramer <jankramer@gmail.com>
2015-12-04 09:09:21 +0100

    added some explanations to lab 2 ex 2 report

--- lab2ex2/runex2.m
+++ lab2ex2/runex2.m
@@ -3,6 +3,7 @@ x = imread('../images/characters.tif');
 
 %% lab 1 ex 2abc
 H = IPgaussian(30, size(x, 1), size(x, 2));
+imwrite(fftshift(H), "filter.png");
 imwrite(IPftfilter(x, H), "characters-GHPF-D30.png");
 H = IPgaussian(60, size(x, 1), size(x, 2));
 imwrite(IPftfilter(x, H), "characters-GHPF-D60.png");
