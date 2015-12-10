b1bca0627f05ec51770c5b2916a5266e0b23512f
Jan Kramer <jankramer@gmail.com>
2015-12-10 16:37:32 +0100

    lab3: inverse dwt2 fixed

--- lab3ex2/IPidwt2.m
+++ lab3ex2/IPidwt2.m
@@ -1,36 +1,39 @@
 % inverse discrete wavelet transform
 function retval = IPidwt2(x, s);
-sqrt4 = sqrt(4);
 out = x;
-[width, height] = size(x);
+
+[height, width] = size(x);
 initrow = height / (2^(s-1));
 initcol = width / (2^(s-1));
 
 for i = 1 : s
-  colsums = out(1:initrow,1:initcol/2);
-  coldiffs = out(1:initrow,initcol/2+1:initcol);
+  mid_r = initrow / 2;
+  mid_c = initcol / 2;
+
+  % make sure we swap horizontal and vertical details
+  rowsums = [out(1:mid_r, 1:mid_c), out(mid_r+1:initrow, 1:mid_c)];
+  rowdiffs = [out(1:mid_r, mid_c+1:initcol), out(mid_r+1:initrow, mid_c+1:initcol)];
   
   % Calculate and scale the result
-  plus = (colsums+coldiffs)/sqrt4;
-  mins = (colsums-coldiffs)/sqrt4;
-  % Combine the new values
-  combined = zeros(initrow,initcol);
-  combined(:,1:2:end) = plus;
-  combined(:,2:2:end) = mins;
-  % Replace the values in the image
-  out(1:initrow,1:initcol) = combined;
-  
-  % Do the same with the rows
-  rowsums = out(1:initrow/2,1:initcol);
-  rowdiffs = out(initrow/2+1,1:initcol);
-  
-  % Calculate and scale the result
-  plus = (rowsums+rowdiffs)/sqrt4;
-  mins = (rowsums-rowdiffs)/sqrt4;
+  plus = (rowsums+rowdiffs);
+  mins = (rowsums-rowdiffs);
   % Combine the new values
   combined = zeros(initrow,initcol);
   combined(1:2:end,:) = plus;
   combined(2:2:end,:) = mins;
+  % Replace the values in the image
+  out(1:initrow,1:initcol) = combined;
+  
+  % Do the same with the columns
+  colsums = out(1:initrow,1:mid_c);
+  coldiffs = out(1:initrow,mid_c+1:initcol);
+  
+  % Calculate and scale the result
+  plus = (colsums+coldiffs);
+  mins = (colsums-coldiffs);
+  % Combine the new values
+  combined(:,1:2:end) = plus;
+  combined(:,2:2:end) = mins;
   % Replace the values in the image
   out(1:initrow,1:initcol) = combined;
   
