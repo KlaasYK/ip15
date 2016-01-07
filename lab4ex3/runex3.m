e96c70d45f574461d6a20c13b1b76a2a3398dad7
Klaas Kliffen <klaasyk@users.noreply.github.com>
2016-01-07 12:02:49 +0100

    Mask finished

--- lab4ex3/runex3.m
+++ lab4ex3/runex3.m
@@ -2,16 +2,28 @@
 x = imread('../images/blobs.tif');
 
 y = makebinary(x,128);
+imwrite(y,'binary.png');
 
 % structuring element
-blob = makeblob(6);
+blob = makeblob(8);
+blob2 = makeblob(11);
+blob3 = makeblob(4);
 
-
-z = makebinary(conv2(y,blob,"valid"),1);
-
-
-imwrite(y,'binary.png');
+% erode the circles
+z = makebinary(conv2(y,blob,"same"),1);
 imwrite(z,'conv.png');
+% take the complement
+a = not(z);
+% erode the background
+b = not(makebinary(conv2(a,blob2,"same"),1));
+imwrite(b,'filtered.png');
+% restore the image (my octave fucked up, must be and)
+c = or(b,  y);
+c = not(makebinary(conv2(not(c),blob3,"same"),1));
+c = or(c,  y);
+
+
+imwrite(c,'restored.png');
 
 
 
