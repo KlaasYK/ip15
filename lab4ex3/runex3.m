cea2b102e620e95f47c0e8ee489bdf49812d416b
Klaas Kliffen <klaasyk@users.noreply.github.com>
2016-01-07 12:37:29 +0100

    lab4ex3 complete, removed noise too

--- lab4ex3/runex3.m
+++ lab4ex3/runex3.m
@@ -4,6 +4,16 @@ x = imread('../images/blobs.tif');
 y = makebinary(x,128);
 imwrite(y,'binary.png');
 
+background = x .* y;
+background2 = 256 - background;
+background2 = background2 .* y;
+background2 = 256 - background2;
+maxnoiseval = max(max(background))
+minnoiseval = min(min(background2))
+meannoise = mean([mean(mean(background)) mean(mean(background2)) ]);
+imwrite(background,'background.png');
+imwrite(background2,'background2.png');
+
 % structuring element
 blob = makeblob(8);
 blob2 = makeblob(11);
@@ -22,8 +32,19 @@ c = or(b,  y);
 c = not(makebinary(conv2(not(c),blob3,"same"),1));
 c = or(c,  y);
 
+%c = xor(c, y);
 
 imwrite(c,'restored.png');
 
+% generate noise image (choose random, or mean)
+noise = rand(size(x))*(maxnoiseval-minnoiseval) + minnoiseval;
+noise = ones(size(x)) * meannoise;
 
+noise = noise .* c;
 
+imwrite(noise,'noise.png');
+
+restore = not(c);
+restoredim = restore .* x + noise;
+
+imwrite(restoredim,'final.png');
